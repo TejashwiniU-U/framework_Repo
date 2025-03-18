4007e6a1ae50c4b5574e2d63789743d36df768c2
Admin <Admin@DESKTOP-0J1MG91>
2025-03-18 11:26:36 -0700

    new project pushed

---
+++ Selenim_Maven_Project/src/test/java/pac_2/OrgTest.java
@@ -0,0 +1,22 @@
+package pac_2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void cretaeorgTest() {
+		
+		System.out.println("execute cretaeorgTest");
+		
+		
+	}
+	
+	@Test
+	public void modifyorgTest() {
+		
+		System.out.println("execute modifyorgTest");
+		
+		
+	}
+
+}
